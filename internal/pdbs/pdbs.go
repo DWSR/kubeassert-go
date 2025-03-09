ae054035753ea11456082f83221c30bb4f592f97
Brandon McNama <brandonmcnama@outlook.com>
2025-03-09 15:37:39 -0400

    wip

---
+++ internal/pdbs/pdbs.go
@@ -0,0 +1,65 @@
+package pdbs
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
+	policyv1 "k8s.io/api/policy/v1"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+	e2etypes "sigs.k8s.io/e2e-framework/pkg/types"
+)
+
+func PodDisruptionBudgetExists(namespaceName, pdbName string) e2etypes.Feature {
+	return features.New("PodDisruptionBudgetExists").
+		WithLabel("type", "pdb").
+		AssessWithDescription(
+			"pdbExists",
+			"PDB should exist",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var pdb policyv1.PodDisruptionBudget
+
+				err := cfg.Client().
+					Resources("poddisruptionbudgets").
+					WithNamespace(namespaceName).
+					Get(ctx, pdbName, namespaceName, &pdb)
+				require.NoError(t, err)
+
+				return ctx
+			}).
+		Feature()
+}
+
+func PodDisruptionBudgetTargetsDeployment(namespaceName, pdbName, deployName string) e2etypes.Feature {
+	return features.New("PodDisruptionBudgetTargetsDeployment").
+		WithLabel("type", "pdb").
+		AssessWithDescription(
+			"pdbTargetsDeployment",
+			"PDB should target deployment",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				var pdb policyv1.PodDisruptionBudget
+				var deploy appsv1.Deployment
+
+				err := cfg.Client().
+					Resources("poddisruptionbudgets").
+					WithNamespace(namespaceName).
+					Get(ctx, pdbName, namespaceName, &pdb)
+				require.NoError(t, err)
+
+				err = cfg.Client().
+					Resources("deployments").
+					WithNamespace(namespaceName).
+					Get(ctx, deployName, namespaceName, &deploy)
+				require.NoError(t, err)
+
+				for labelKey, labelValue := range pdb.Spec.Selector.MatchLabels {
+					require.Equal(t, deploy.Spec.Selector.MatchLabels, labelKey)
+					require.Equal(t, deploy.Spec.Selector.MatchLabels[labelKey], labelValue)
+				}
+
+				return ctx
+			}).
+		Feature()
+}
