75cecd51789cec78da5498e94e411de93868ed34
Brandon McNama <brandonmcnama@outlook.com>
2025-03-09 20:53:49 -0400

    wip

--- internal/assertion/assertion_options.go
+++ internal/assertion/assertion_options.go
@@ -11,42 +11,49 @@ import (
 // Option is a function that configures one or more facets of an Assertion.
 type Option func(Assertion)
 
+// WithResourceLabels sets the labels to be used when selecting resources for the assertion.
 func WithResourceLabels(labels map[string]string) Option {
 	return func(a Assertion) {
 		a.setLabels(labels)
 	}
 }
 
+// WithResourceFields sets the fields to be used when selecting resources for the assertion.
 func WithResourceFields(fields map[string]string) Option {
 	return func(a Assertion) {
 		a.setFields(fields)
 	}
 }
 
+// WithInterval sets the interval used when polling for the assertion to be true.
 func WithInterval(interval time.Duration) Option {
 	return func(a Assertion) {
 		a.setInterval(interval)
 	}
 }
 
+// WithTimeout sets the timeout used when polling for the assertion to be true.
 func WithTimeout(timeout time.Duration) Option {
 	return func(a Assertion) {
 		a.setTimeout(timeout)
 	}
 }
 
+// WithBuilder sets the features.FeatureBuilder used to build the e2e-framework Feature.
 func WithBuilder(builder *features.FeatureBuilder) Option {
 	return func(a Assertion) {
 		a.SetBuilder(builder)
 	}
 }
 
+// WithRequireT sets the require.TestingT used to report test failures.
 func WithRequireT(requireT require.TestingT) Option {
 	return func(a Assertion) {
 		a.setRequireT(requireT)
 	}
 }
 
+// WithResourceNamespace sets the namespace to be used when selecting resources for the assertion.
 func WithResourceNamespace(namespaceName string) Option {
 	return func(a Assertion) {
 		newFields := a.GetFields()
@@ -55,12 +62,15 @@ func WithResourceNamespace(namespaceName string) Option {
 	}
 }
 
+// WithResourceNamespaceFromTestEnv sets the namespace to be used when selecting resources for the assertion to the
+// namespace set in the test environment.
 func WithResourceNamespaceFromTestEnv() Option {
 	return func(a Assertion) {
 		a.setListOptionsFn(listOptionsWithNamespaceFromEnv)
 	}
 }
 
+// WithResourceName sets the name (i.e. metadata.name) to be used when selecting resources for the assertion.
 func WithResourceName(name string) Option {
 	return func(a Assertion) {
 		newFields := a.GetFields()
@@ -69,22 +79,28 @@ func WithResourceName(name string) Option {
 	}
 }
 
+// WithSetup adds setup steps to the assertion.
 func WithSetup(steps ...e2etypes.StepFunc) Option {
-	return func(a Assertion) {
-		builder := a.GetBuilder()
+	return func(assert Assertion) {
+		builder := assert.GetBuilder()
+
 		for _, s := range steps {
 			builder = builder.Setup(s)
 		}
-		a.SetBuilder(builder)
+
+		assert.SetBuilder(builder)
 	}
 }
 
+// WithTeardown adds teardown steps to the assertion.
 func WithTeardown(steps ...e2etypes.StepFunc) Option {
-	return func(a Assertion) {
-		builder := a.GetBuilder()
+	return func(assert Assertion) {
+		builder := assert.GetBuilder()
+
 		for _, s := range steps {
 			builder = builder.Teardown(s)
 		}
-		a.SetBuilder(builder)
+
+		assert.SetBuilder(builder)
 	}
 }
--- internal/testhelpers/test_asserts.go
+++ internal/testhelpers/test_asserts.go
@@ -12,36 +12,42 @@ import (
 )
 
 type (
+	// SuccessfulAssert is a struct that contains the name of the test and a function that returns an assertion.Assertion
+	// that should be true or pass.
 	SuccessfulAssert struct {
 		Name             string
 		SuccessfulAssert func(t require.TestingT) assertion.Assertion
 	}
 
+	// FailingAssert is a struct that contains the name of the test and a function that returns an assertion.Assertion
+	// that should be false or fail.
 	FailingAssert struct {
 		Name          string
 		FailingAssert func(t require.TestingT) assertion.Assertion
 	}
 )
 
+// TestSuccessfulAsserts is a helper function that runs a series of successful asserts.
 func TestSuccessfulAsserts(t *testing.T, testEnv env.Environment, asserts ...SuccessfulAssert) {
 	t.Helper()
 
 	assertFeatures := make([]features.Feature, 0)
 
 	for _, a := range asserts {
-		assertFeatures = append(assertFeatures, a.SuccessfulAssert(t).AsFeature())
+		assertFeatures = append(assertFeatures, assertion.AsFeature(a.SuccessfulAssert(t)))
 	}
 
 	testEnv.Test(t, assertFeatures...)
 }
 
+// TestFailingAsserts is a helper function that runs a series of failing asserts.
 func TestFailingAsserts(t *testing.T, testEnv env.Environment, asserts ...FailingAssert) {
 	t.Helper()
 
 	for _, tc := range asserts {
 		t.Run(tc.Name, func(t *testing.T) {
 			mockT := &MockT{}
-			testEnv.Test(t, tc.FailingAssert(mockT).AsFeature())
+			testEnv.Test(t, assertion.AsFeature(tc.FailingAssert(mockT)))
 			assert.True(t, mockT.Failed)
 		})
 	}
