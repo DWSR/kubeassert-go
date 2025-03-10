d0210c362a500556a882271f267fb2b4e783eafb
Brandon McNama <brandonmcnama@outlook.com>
2025-03-09 20:11:37 -0400

    wip

--- internal/pdbs/pdbs.go
+++ internal/pdbs/pdbs.go
@@ -5,12 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	appsv1 "k8s.io/api/apps/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
-	e2etypes "sigs.k8s.io/e2e-framework/pkg/types"
 
 	"github.com/DWSR/kubeassert-go/internal/assertion"
 	helpers "github.com/DWSR/kubeassert-go/internal/assertionhelpers"
@@ -50,10 +48,6 @@ func (pa PDBAssertion) getPDBs(
 }
 
 func (pa PDBAssertion) Exists() PDBAssertion {
-	return pa.ExactlyNExist(1)
-}
-
-func (pa PDBAssertion) ExactlyNExist(count int) PDBAssertion {
 	stepFn := func(ctx context.Context, testingT *testing.T, cfg *envconf.Config) context.Context {
 		t := helpers.RequireTIfNotNil(testingT, pa.GetRequireT())
 
@@ -61,7 +55,7 @@ func (pa PDBAssertion) ExactlyNExist(count int) PDBAssertion {
 			pdbs, err := pa.getPDBs(ctx, t, cfg)
 			require.NoError(t, err)
 
-			return len(pdbs.Items) == count, nil
+			return len(pdbs.Items) == 1, nil
 		}
 
 		require.NoError(t, pa.WaitForCondition(ctx, conditionFunc))
@@ -69,61 +63,9 @@ func (pa PDBAssertion) ExactlyNExist(count int) PDBAssertion {
 		return ctx
 	}
 	res := pa.clone()
-	res.SetBuilder(res.GetBuilder().Assess("exactlyNExist", stepFn))
+	res.SetBuilder(res.GetBuilder().Assess("exists", stepFn))
 
 	return res
-}
-
-func PodDisruptionBudgetExists(namespaceName, pdbName string) e2etypes.Feature {
-	return features.New("PodDisruptionBudgetExists").
-		WithLabel("type", "pdb").
-		AssessWithDescription(
-			"pdbExists",
-			"PDB should exist",
-			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-				var pdb policyv1.PodDisruptionBudget
-
-				err := cfg.Client().
-					Resources("poddisruptionbudgets").
-					WithNamespace(namespaceName).
-					Get(ctx, pdbName, namespaceName, &pdb)
-				require.NoError(t, err)
-
-				return ctx
-			}).
-		Feature()
-}
-
-func PodDisruptionBudgetTargetsDeployment(namespaceName, pdbName, deployName string) e2etypes.Feature {
-	return features.New("PodDisruptionBudgetTargetsDeployment").
-		WithLabel("type", "pdb").
-		AssessWithDescription(
-			"pdbTargetsDeployment",
-			"PDB should target deployment",
-			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-				var pdb policyv1.PodDisruptionBudget
-				var deploy appsv1.Deployment
-
-				err := cfg.Client().
-					Resources("poddisruptionbudgets").
-					WithNamespace(namespaceName).
-					Get(ctx, pdbName, namespaceName, &pdb)
-				require.NoError(t, err)
-
-				err = cfg.Client().
-					Resources("deployments").
-					WithNamespace(namespaceName).
-					Get(ctx, deployName, namespaceName, &deploy)
-				require.NoError(t, err)
-
-				for labelKey, labelValue := range pdb.Spec.Selector.MatchLabels {
-					require.Equal(t, deploy.Spec.Selector.MatchLabels, labelKey)
-					require.Equal(t, deploy.Spec.Selector.MatchLabels[labelKey], labelValue)
-				}
-
-				return ctx
-			}).
-		Feature()
 }
 
 func NewPDBAssertion(opts ...assertion.AssertionOption) PDBAssertion {
