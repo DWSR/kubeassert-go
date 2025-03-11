f247f30b011bdd82213e4be55b6fcb023d2ac1cc
Brandon McNama <brandonmcnama@outlook.com>
2025-03-11 08:32:19 -0400

    wip

--- internal/namespaces/namespaces.go
+++ internal/namespaces/namespaces.go
@@ -2,38 +2,23 @@ package namespaces
 
 import (
 	"context"
-	"testing"
 
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
-	"sigs.k8s.io/e2e-framework/pkg/features"
 
 	"github.com/DWSR/kubeassert-go/internal/assertion"
 	helpers "github.com/DWSR/kubeassert-go/internal/assertionhelpers"
 )
 
-type NamespaceAssertion struct {
-	assertion.Assertion
-}
-
-const (
-	podSecurityEnforceLabelKey = "pod-security.kubernetes.io/enforce"
-)
-
-func (na NamespaceAssertion) clone() NamespaceAssertion {
-	return NamespaceAssertion{
-		Assertion: assertion.Clone(na.Assertion),
-	}
-}
-
-func (na NamespaceAssertion) getNamespaces(ctx context.Context, t require.TestingT, cfg *envconf.Config) (corev1.NamespaceList, error) {
+func getNamespaces(ctx context.Context, t require.TestingT, cfg *envconf.Config, listOpts metav1.ListOptions) (corev1.NamespaceList, error) {
 	client := helpers.DynamicClientFromEnvconf(t, cfg)
 
 	var nsList corev1.NamespaceList
 
-	list, err := client.Resource(corev1.SchemeGroupVersion.WithResource("namespaces")).List(ctx, na.ListOptions(cfg))
+	list, err := client.Resource(corev1.SchemeGroupVersion.WithResource("namespaces")).List(ctx, listOpts)
 	if err != nil {
 		return nsList, err
 	}
@@ -46,135 +31,19 @@ func (na NamespaceAssertion) getNamespaces(ctx context.Context, t require.Testin
 	return nsList, nil
 }
 
-func (na NamespaceAssertion) Exists() NamespaceAssertion {
-	return na.ExactlyNExist(1)
-}
-
-func (na NamespaceAssertion) ExactlyNExist(count int) NamespaceAssertion {
-	fn := func(ctx context.Context, testingT *testing.T, cfg *envconf.Config) context.Context {
-		t := helpers.RequireTIfNotNil(testingT, na.GetRequireT())
-		conditionFunc := func(ctx context.Context) (bool, error) {
-			nsList, err := na.getNamespaces(ctx, t, cfg)
+func exist() helpers.ConditionFuncFactory {
+	return func(
+		t require.TestingT,
+		assert assertion.Assertion,
+		cfg *envconf.Config,
+		count int,
+		itemCountFn, _ helpers.IntCompareFunc,
+	) helpers.ConditionFunc {
+		return func(ctx context.Context) (bool, error) {
+			secrets, err := getNamespaces(ctx, t, cfg, assert.ListOptions(cfg))
 			require.NoError(t, err)
 
-			return len(nsList.Items) == count, nil
+			return itemCountFn(len(secrets.Items), count), nil
 		}
-
-		require.NoError(t, helpers.WaitForCondition(ctx, na, conditionFunc))
-
-		return ctx
-	}
-
-	res := na.clone()
-	res.SetBuilder(res.GetBuilder().Assess("exists", fn))
-
-	return res
-}
-
-func (na NamespaceAssertion) AtLeastNExist(count int) NamespaceAssertion {
-	fn := func(ctx context.Context, testingT *testing.T, cfg *envconf.Config) context.Context {
-		t := helpers.RequireTIfNotNil(testingT, na.GetRequireT())
-		conditionFunc := func(ctx context.Context) (bool, error) {
-			nsList, err := na.getNamespaces(ctx, t, cfg)
-			require.NoError(t, err)
-
-			return len(nsList.Items) >= count, nil
-		}
-
-		require.NoError(t, helpers.WaitForCondition(ctx, na, conditionFunc))
-
-		return ctx
-	}
-
-	res := na.clone()
-	res.SetBuilder(res.GetBuilder().Assess("exists", fn))
-
-	return res
-}
-
-func (na NamespaceAssertion) AtLeastNAreRestricted(count int) NamespaceAssertion {
-	fn := func(ctx context.Context, testingT *testing.T, cfg *envconf.Config) context.Context {
-		t := helpers.RequireTIfNotNil(testingT, na.GetRequireT())
-		conditionFunc := func(ctx context.Context) (bool, error) {
-			nsList, err := na.getNamespaces(ctx, t, cfg)
-			require.NoError(t, err)
-
-			if len(nsList.Items) < count {
-				return false, nil
-			}
-
-			restrictedCount := 0
-
-			for _, ns := range nsList.Items {
-				nsLabels := ns.GetLabels()
-
-				enforceLabel, ok := nsLabels[podSecurityEnforceLabelKey]
-				if ok && enforceLabel == "restricted" {
-					restrictedCount += 1
-				}
-			}
-
-			return restrictedCount >= count, nil
-		}
-
-		require.NoError(t, helpers.WaitForCondition(ctx, na, conditionFunc))
-
-		return ctx
-	}
-
-	res := na.clone()
-	res.SetBuilder(na.GetBuilder().Assess("atLeastNAreRestricted", fn))
-
-	return res
-}
-
-func (na NamespaceAssertion) IsRestricted() NamespaceAssertion {
-	return na.ExactlyNAreRestricted(1)
-}
-
-func (na NamespaceAssertion) ExactlyNAreRestricted(count int) NamespaceAssertion {
-	fn := func(ctx context.Context, testingT *testing.T, cfg *envconf.Config) context.Context {
-		t := helpers.RequireTIfNotNil(testingT, na.GetRequireT())
-		conditionFunc := func(ctx context.Context) (bool, error) {
-			nsList, err := na.getNamespaces(ctx, t, cfg)
-			require.NoError(t, err)
-
-			if len(nsList.Items) < count {
-				return false, nil
-			}
-
-			restrictedCount := 0
-
-			for _, ns := range nsList.Items {
-				nsLabels := ns.GetLabels()
-
-				enforceLabel, ok := nsLabels[podSecurityEnforceLabelKey]
-				if ok && enforceLabel == "restricted" {
-					restrictedCount += 1
-				}
-			}
-
-			return restrictedCount == count, nil
-		}
-
-		require.NoError(t, helpers.WaitForCondition(ctx, na, conditionFunc))
-
-		return ctx
-	}
-
-	res := na.clone()
-	res.SetBuilder(na.GetBuilder().Assess("atLeastNAreRestricted", fn))
-
-	return res
-}
-
-func NewNamespaceAssertion(opts ...assertion.Option) NamespaceAssertion {
-	return NamespaceAssertion{
-		Assertion: assertion.NewAssertion(
-			append(
-				[]assertion.Option{assertion.WithBuilder(features.New("Namespace").WithLabel("type", "namespace"))},
-				opts...,
-			)...,
-		),
 	}
 }
