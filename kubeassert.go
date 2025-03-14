10c314362b2d597856eba581ce64ad9558af5454
Brandon McNama <brandonmcnama@outlook.com>
2025-03-13 22:31:15 -0400

    fix: re-export Assertion interface (#4)
    Fixes a regression where the Assertion interface is no longer exported
    from the package.

--- kubeassert.go
+++ kubeassert.go
@@ -15,6 +15,7 @@ import (
 )
 
 type (
+	Assertion           = assertion.Assertion
 	DeploymentAssertion = deployments.DeploymentAssertion
 	NamespaceAssertion  = namespaces.NamespaceAssertion
 	CRDAssertion        = crds.CRDAssertion
